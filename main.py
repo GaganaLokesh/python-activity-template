48959854ee0fcebba112be34c3fdcf4f36e7f1fe
GaganaLokesh <142224773+GaganaLokesh@users.noreply.github.com>
2024-10-01 13:53:31 +0000

    7th

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 file=open("example1.txt","a")
 file.write("\nDear Dairy")
 file.close()
+with open("example1.txt", "r") as file:
+    content = file.read()
+print("File automatically closed after exiting the block.")
