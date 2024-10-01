f9b2f855c4ba96a4138cce1b9c83ee124f51a663
GaganaLokesh <142224773+GaganaLokesh@users.noreply.github.com>
2024-10-01 13:54:52 +0000

    8th

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-file=open("example1.txt","a")
-file.write("\nDear Dairy")
-file.close()
-with open("example1.txt", "r") as file:
-    content = file.read()
-print("File automatically closed after exiting the block.")
+try:
+    file = open("nonexistent.txt", "r")
+except FileNotFoundError:
+    print("The file does not exist.")
